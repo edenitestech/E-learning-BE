541cd68623562d6f406a774cc44b381e804d666e
Strongcode <emmanueludoh360@gmail.com>
2025-05-02 08:53:46 -0700

    added redirects to root api

--- edenites_be/urls.py
+++ edenites_be/urls.py
@@ -22,15 +22,18 @@ from content.views import LessonViewSet, QuestionViewSet
 from enrollments.views import EnrollmentViewSet
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register("categories", CategoryViewSet)
 router.register("courses", CourseViewSet)
 router.register("lessons", LessonViewSet)
 router.register("questions", QuestionViewSet)
 router.register("enrollments", EnrollmentViewSet, basename="enrollments")
 
+
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
     path("api/auth/", include("accounts.urls")),
